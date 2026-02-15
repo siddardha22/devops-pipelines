571c7113ec78c8237774bdf736e2d79f87bdcea0
siddu <konurusiddardh@gmail.com>
2026-02-15 10:19:36 +0530

    feat: add CI-PIPELINE

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log(" hello world");
+console.log(" learning ci cd pipelines");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting tests.........");
+setTimeout(() => {
+    console.log("tests completed successfully");
+}, 2000);
+console.log("testing completed");
